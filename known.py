75f9efccc2da9519e2e4dcff8731a9330a3a6bbb
Michalis Panayides <PanayidesM@cardiff.ac.uk>
2021-04-28 10:28:00 +0100

    Update known.py

--- known.py
+++ known.py
@@ -1,9 +1,14 @@
 """ A set of known words and end-of-word patterns for the spellchecker. """
 
 words = {
+    "automata",
+    "calc",
     "HB",
     "ieeetr",
+    "math",
+    "memoryless",
     "Michalis",
     "Panayides",
+    "pre",
     "queueing",
 }
