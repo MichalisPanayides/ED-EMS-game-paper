3bac21070266971181409479be81423c406c7d0e
Michalis Panayides <PanayidesM@cardiff.ac.uk>
2021-09-15 14:47:38 +0100

    Section: ED-EMS application (#11)
    * Add ARD with PoA plots in imgs/ directory
    
    Each added pdf has four plots on it:
    - Asymmetric replicator dynamics stratgies for row player
    - Asymmetric replicator dynamics stratgies for column player
    - Blocking time price of anarchy over time for row player
    - Blocking time price of anarchy over time for column player
    
    * Add initial draft of Application section
    
    * Update known words
    
    * Add info about asymmetric replicator dynamics
    
    * Improve asymmetric rd plots
    
    Increase the granularity of the PoA curve so that it looks nicer
    
    * Add jupyter notebook for the base case of ard run
    
    The jupyter notebook shows some additional details of the generated
    plot.
    
    * Fix figure labels and include some citations
    
    * Add extra checks (#12)
    
    * Update environment to include proselint
    
    * Add proselint check as a task
    
    * Update CI to run proselint check
    
    * Make changes suggested by proselint
    
    * Fix typo in ci.yml
    
    * Add alex to github actions checks
    
    * Apply changes based on Alex's suggestions
    
    * Include alex check as a task
    
    * Apply alex's suggestion
    
    * Add ARD with PoA plots in imgs/ directory
    
    Each added pdf has four plots on it:
    - Asymmetric replicator dynamics stratgies for row player
    - Asymmetric replicator dynamics stratgies for column player
    - Blocking time price of anarchy over time for row player
    - Blocking time price of anarchy over time for column player
    
    * Add initial draft of Application section
    
    * Update known words
    
    * Add info about asymmetric replicator dynamics
    
    * Improve asymmetric rd plots
    
    Increase the granularity of the PoA curve so that it looks nicer
    
    * Add jupyter notebook for the base case of ard run
    
    The jupyter notebook shows some additional details of the generated
    plot.
    
    * Fix figure labels and include some citations
    
    * Compile main.pdf
    
    * Apply changes based on alex and proselint
    
    * Add .alexrc.yml file
    
    This is a configuration file for alex so that certain of its suggestions
    are ignored. In this case the blocking time (labelled as B_i) was found
    as an incensitive phrase.
    
    * Apply suggestions from code review
    
    Co-authored-by: Vince Knight <vincent.knight@gmail.com>
    
    * Apply minor changes
    
    * Update known words for spellchecker
    
    * More changes form code review and fix minor typos
    
    * Add small paragraph of news articles
    
    * Add more around analysing games
    
    - Include short Nash equilibrium paragraph and why we will not use it
    - Include small numeric example of NE
    - Include mathematics around asymmetric replicator dynamics
    
    * Apply proselint suggestion
    
    * Add dicussion for price of anarchy
    
    * Include payoff matrices
    
    * Update known words
    
    * Fix typo
    
    * Apply changes based on supervisory meeting
    
    Co-authored-by: Vince Knight <vincent.knight@gmail.com>

---
+++ imgs/asymmetric_rd/main.py
@@ -0,0 +1,58 @@
+import nashpy as nash
+import numpy as np
+import matplotlib.pyplot as plt
+
+import ambulance_game as abg
+
+lambda_2 = 2
+lambda_1_1 = 1
+lambda_1_2 = 2
+mu_1 = 2
+mu_2 = 2.5
+num_of_servers_1 = 2
+num_of_servers_2 = 2
+system_capacity_1 = 10
+system_capacity_2 = 10
+buffer_capacity_1 = 6
+buffer_capacity_2 = 6
+target = 2
+alpha = 0.5
+
+
+A, B, R = abg.game.get_payoff_matrices(
+    lambda_2=lambda_2,
+    lambda_1_1=lambda_1_1,
+    lambda_1_2=lambda_1_2,
+    mu_1=mu_1,
+    mu_2=mu_2,
+    num_of_servers_1=num_of_servers_1,
+    num_of_servers_2=num_of_servers_2,
+    system_capacity_1=system_capacity_1,
+    system_capacity_2=system_capacity_2,
+    buffer_capacity_1=buffer_capacity_1,
+    buffer_capacity_2=buffer_capacity_2,
+    target=target,
+    alpha=alpha,
+)
+A += 1
+B += 1
+
+my_game = nash.Game(A, B)
+xs, ys = my_game.asymmetric_replicator_dynamics(timepoints=np.linspace(1, 200000, 100))
+
+plt.figure(figsize=(15, 5))
+plt.subplot(1, 2, 1)
+plt.plot(xs)
+plt.xlabel("Timepoints")
+plt.ylabel("Probability")
+plt.title("Row player")
+plt.legend([f"$s_{{{i + 1}}}$" for i in range(len(xs[0]))])
+
+plt.subplot(1, 2, 2)
+plt.plot(ys)
+plt.xlabel("Timepoints")
+plt.ylabel("Probability")
+plt.title("Column player")
+plt.legend([f"$s_{{{i + 1}}}$" for i in range(len(ys[0]))])
+
+plt.savefig("asymmetric_rd.png", transparent=True)
