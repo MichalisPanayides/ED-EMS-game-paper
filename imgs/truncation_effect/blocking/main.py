b54e4ad5603343078f848e43562f4b66f40b1a82
Michalis <PanayidesM@cardiff.ac.uk>
2022-03-03 11:49:40 +0000

    Add bounded simulation to same plot

--- imgs/truncation_effect/blocking/main.py
+++ imgs/truncation_effect/blocking/main.py
@@ -1,3 +1,5 @@
+from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
 import matplotlib.pyplot as plt
 import numpy as np
 import ambulance_game as abg
@@ -10,19 +12,21 @@ seed_num = 1
 warm_up_time = 100
 num_of_trials = 20
 runtime = 10000
-# warm_up_time = 10
-# num_of_trials = 10
-# runtime = 100
 
-all_sim_blocking_times = []
+lambda_2_space = np.linspace(2, 6, 6)
+all_capacity_values = (10, 30, 50)
+
+all_sim_unbound_blocking_times = []
+all_sim_bound_blocking_times_low = []
+all_sim_bound_blocking_times_mid = []
+all_sim_bound_blocking_times_high = []
+
 all_markov_blocking_times_low = []
 all_markov_blocking_times_mid = []
 all_markov_blocking_times_high = []
-lambda_2_space = np.linspace(2, 6, 6)
-all_capacity_values = (10, 30, 50)
 for lambda_2 in lambda_2_space:
-    # Simulation
-    simulation_results = abg.simulation.get_multiple_runs_results(
+    # Unbounded simulation
+    unbounded_simulation_results = abg.simulation.get_multiple_runs_results(
         lambda_2=lambda_2,
         lambda_1=lambda_1,
         mu=mu,
@@ -36,12 +40,12 @@ for lambda_2 in lambda_2_space:
         runtime=runtime,
         class_type=None,
     )
-    all_sim_blocking_times.append(
-        [np.mean(b.blocking_times) for b in simulation_results]
+    all_sim_unbound_blocking_times.append(
+        [np.mean(b.blocking_times) for b in unbounded_simulation_results]
     )
 
-    # Markov
     for capacity_value in all_capacity_values:
+        # Markov
         markov_blocking_time = (
             abg.markov.get_mean_blocking_time_using_markov_state_probabilities(
                 lambda_2=lambda_2,
@@ -53,18 +57,43 @@ for lambda_2 in lambda_2_space:
                 buffer_capacity=capacity_value,
             )
         )
+        # Bounded simulation
+        bounded_simulation_results = abg.simulation.get_multiple_runs_results(
+            lambda_2=lambda_2,
+            lambda_1=lambda_1,
+            mu=mu,
+            num_of_servers=num_of_servers,
+            threshold=threshold,
+            system_capacity=capacity_value,
+            buffer_capacity=capacity_value,
+            seed_num=seed_num,
+            warm_up_time=warm_up_time,
+            num_of_trials=num_of_trials,
+            runtime=runtime,
+            class_type=None,
+        )
+
         if capacity_value == all_capacity_values[0]:
             all_markov_blocking_times_low.append(markov_blocking_time)
+            all_sim_bound_blocking_times_low.append(
+                [np.mean(w.blocking_times) for w in bounded_simulation_results]
+            )
         elif capacity_value == all_capacity_values[1]:
             all_markov_blocking_times_mid.append(markov_blocking_time)
+            all_sim_bound_blocking_times_mid.append(
+                [np.mean(w.blocking_times) for w in bounded_simulation_results]
+            )
         elif capacity_value == all_capacity_values[2]:
             all_markov_blocking_times_high.append(markov_blocking_time)
+            all_sim_bound_blocking_times_high.append(
+                [np.mean(w.blocking_times) for w in bounded_simulation_results]
+            )
         else:
             raise ValueError("Invalid capacity value")
 
 # Waiting time both individuals
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
-for index, (ax, all_markov_times) in enumerate(
+for index, (ax, all_markov_times, all_bounded_sim_times) in enumerate(
     zip(
         [ax1, ax2, ax3],
         [
@@ -72,20 +101,40 @@ for index, (ax, all_markov_times) in enumerate(
             all_markov_blocking_times_mid,
             all_markov_blocking_times_high,
         ],
+        [
+            all_sim_bound_blocking_times_low,
+            all_sim_bound_blocking_times_mid,
+            all_sim_bound_blocking_times_high,
+        ],
     )
 ):
     ax.violinplot(
-        all_sim_blocking_times, positions=lambda_2_space, showmeans=True, widths=0.3
+        all_sim_unbound_blocking_times,
+        positions=lambda_2_space,
+        showmeans=True,
+        widths=0.3,
+    )
+    ax.violinplot(
+        all_bounded_sim_times,
+        positions=lambda_2_space,
+        showmeans=True,
+        widths=0.3,
     )
     ax.plot(
         lambda_2_space,
         all_markov_times,
         color="red",
     )
+
+    legend_elements = [
+        Line2D([0], [0], color="red", lw=2, label="Markov Chain"),
+        Patch(facecolor="tab:blue", label="Simulation (Unbounded)", alpha=0.6),
+        Patch(facecolor="tab:orange", label="Simulation (Bounded)", alpha=0.6),
+    ]
+    ax.legend(handles=legend_elements, loc="upper left")
     ax.set_title(f"$N$ = $M$ = {all_capacity_values[index]}")
-    ax.legend(["Markov chain", "Simulation"], loc="upper left")
     ax.set_xlabel("$\lambda_2$")
     ax.set_ylabel("Blocking time")
 
 plt.suptitle("Truncation effect on blocking time")
-plt.savefig("main.pdf", transparent=True)
+plt.savefig("demo.pdf", transparent=True)
